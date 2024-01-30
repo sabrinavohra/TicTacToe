d1544b53bc2de4646a9f42fe5c69caa15e6fd8a1
sabrinavohra <sabrina.vohra@menloschool.org>
2024-01-30 09:55:06 -0800

    Set instance variables and worked on constructor

--- src/TicTacToeViewer.java
+++ src/TicTacToeViewer.java
@@ -1,3 +1,16 @@
-public class TicTacToeViewer {
+import javax.swing.JFrame;
+public class TicTacToeViewer implements JFrame{
     // TODO: Complete this class
+    private static final int WINDOW_HEIGHT = 800;
+    private static final int WINDOW_WIDTH = 600;
+    private static final int SIDE_LENGTH = 20;
+    private static final int X_BUFFER = 40;
+    private static final int Y_BUFFER = 50;
+
+    public TicTacToeViewer() {
+        this.setSize(WINDOW_HEIGHT, WINDOW_WIDTH);
+        this.setTitle("TicTacToe");
+        this.setDefaultCloseOperation("JFrame.EXIT_ON_CLOSE");
+        this.setVisible(true);
+    }
 }
